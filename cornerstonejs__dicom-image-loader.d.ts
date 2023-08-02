da40a93c5300c8d495db144c918a154e49cde3fa
juanEurquijo <juancho.urquijo@gmail.com>
2023-08-01 21:54:12 -0500

    added image render

---
+++ cornerstonejs__dicom-image-loader.d.ts
@@ -0,0 +1,1 @@
+declare module '@cornerstonejs/dicom-image-loader';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import * as cornerstone from 'cornerstone-core';
+// @ts-ignore
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testMicro';
+  imageId =
+    'wadouri:https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323563.dcm';
+
+  ngOnInit(): void {
+    var element = document.getElementById('element');
+
+    // Configure WADO Image Loader with dicomParser
+    cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+    cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+
+    cornerstoneWADOImageLoader.webWorkerManager.initialize({
+      maxWebWorkers: navigator.hardwareConcurrency || 1,
+      startWebWorkersOnDemand: false,
+      taskConfiguration: {
+        decodeTask: {
+          initializeCodecsOnStartup: true,
+          strict: false,
+        },
+      },
+    });
+
+    cornerstone.enable(element!);
+
+    // Load & Display
+    cornerstone.loadImage(this.imageId).then(function (image: cornerstone.Image) {
+
+      cornerstone.displayImage(element!, image);
+
+      var viewport = {
+        invert: false,
+        pixelReplication: false,
+        voi: {
+          windowWidth: 400,
+          windowCenter: 100,
+        },
+        scale: 1.4,
+        translation: {
+          x: 0,
+          y: 0,
+        },
+      };
+
+      cornerstone.setViewport(element!, viewport);
+      cornerstone.updateImage(element!);
+    });
+  }
+
 }
